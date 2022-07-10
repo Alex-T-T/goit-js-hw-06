64da0a331ea7e2696610a47cbf726db8faa869e9
Olexandr Tolochko <0504fusion@gmail.com>
2022-07-10 17:18:21 +0300

    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
+const form = document.querySelector('.login-form');
+const inputs = document.querySelectorAll('input');
+const email = inputs[0];
+const password = inputs[1];
+
+const getInfo = (event) => {
+    event.preventDefault();
+    const infoBox = {
+        email: email.value,
+        password: password.value,
+    };
+
+     if (email.value === '' || password.value === '') {
+        alert('Додайте дані в порожні поля: email/password');
+    } 
+    // if (email.value === '') {
+    //     alert('Додайте дані в поле email');
+    // } 
+
+    //  if (password.value === '') {
+    //     alert('Будь-ласка, заповніть пусте поле password');
+    // } 
+
+    console.log(infoBox);
+    form.reset();
+};
+    
+form.addEventListener('submit', getInfo);
+
+
+
+
+
+
+
+
 
